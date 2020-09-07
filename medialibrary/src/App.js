e4e2c5dfe809fa5c82682cbf99a2cb3e297730e7
Tom <demagny.t@hotmail.fr>
2020-09-07 17:25:08 +0200

    modification App.js

--- medialibrary/src/App.js
+++ medialibrary/src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
+import Author from './components/Author';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+        <Router>
+          <NavBar />
+          <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/author" component={Author}/>
+            <Route
+                path="/"
+                component={() =>
+                    <p>Oups cette page n'existe pas</p>}
+            />
+          </Switch>
+        </Router>
+
   );
 }
 
